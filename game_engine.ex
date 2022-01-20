9faa6d99d0ea3a5b84e5a29a4eb9f9804c16ce5d
Dan Gutt <dgutt10@gmail.com>
2022-01-19 20:53:42 -0600

    Small update. Cleaning up engine to start updating state

--- game_engine.ex
+++ game_engine.ex
@@ -11,20 +11,29 @@ defmodule GameEngine do
   end
 
   def run(state_pid) do
-    current_state = GameState.get(state_pid)
-    free_set = current_state[:free]
-    x_set = current_state[:x]
-    o_set = current_state[:o]
+    {free_set, x_set, o_set, player_1, player_2} = get_state(state_pid)
     # Draw board
     IO.puts(GameBoard.draw(free_set, x_set, o_set))
     # First player picks
-    player_1_square = IO.gets("#{current_state[:player_1]} pick your square: ")
-    IO.puts("Player 1 number picked: #{player_1_square}")
+    player_1_square = IO.gets("#{player_1} pick your square: ")
     # Second Player picks
     # check if anyone won
     # if end state reached -> restart game
     # else repeat
     run(state_pid)
+  end
+
+  defp get_state(state_pid) do 
+    current_state = GameState.get(state_pid)
+    free_set = current_state[:free]
+    x_set = current_state[:x]
+    o_set = current_state[:o]
+    player_1 = current_state[:player_1]
+    player_2 = current_state[:player_2]
+    {free_set, x_set, o_set, player_1, player_2}
+  end
+
+  defp set_state do 
   end
 end
 
